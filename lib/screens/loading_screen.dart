79df375d082ce820e4cefcf4a218e30d9f9477e8
TshephangMogotsi <zinjie7@gmail.com>
2022-07-24 13:53:22 +0200

    updated minSDK to 33 for the geolocator dependency

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -8,6 +8,10 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  void getLocation(){
+
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
